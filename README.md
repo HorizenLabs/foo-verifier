1c28c9f679a7ec1b29269a9ac4bb13c9943133c7
JackPiri <gpirinoli@horizenlabs.io>
2024-07-30 14:22:21 +0200

    Dummy implementation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# foo-verifier
+# Foo verifier
+
+A dummy verifier. It acts as a placeholder for the tutorial related to the integration of a new verifier into zkVerify.
---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+// Copyright 2024, The Horizen Foundation
+
+/// The verification key
+pub type Vk = [u8; 32];
+
+/// The proof
+pub type Proof = [u8; 512];
+
+/// The public inputs
+pub type PublicInputs = [u8; 32];
+
+pub enum VerifyError {
+    /// Failure
+    Failure,
+}
+
+/// Perform dummy verification; a trivial condition for raising error is provided
+pub fn verify(vk: Vk, proof: Proof, pubs: PublicInputs) -> Result<(), VerifyError> {
+    if *vk.last().unwrap() == 0u8 && *proof.last().unwrap() == 0u8 && *pubs.last().unwrap() == 0u8 {
+        return Err(VerifyError::Failure);
+    }
+    Ok(())
+}
